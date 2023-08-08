5121930cc867b70b80d58ef863461e6822404926
chuma-beep <wisboynelson123@gmail.com>
2023-08-08 14:12:23 +0100

    + 2

---
+++ index.js
@@ -0,0 +1,15 @@
+//retrun the sum of two numbers.
+//create  a function that take stwo numbers  as argumemts and returns their sum.
+function addition(a,b){
+    return a + b;
+}
+console.log(addition(3, 2));
+
+//retun the next number from the integer
+// create a function that take aa number as an arguement, increments the number by +1 and returns the result.
+function addition(num){
+    return num++
+}
+
+//power calculator
+//create a  function that takes voltage and current and returns the calculated power.
